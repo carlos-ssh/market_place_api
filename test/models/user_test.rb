ca2c33708fbb91328305f1ea665c0f4f3bc82979
Carlos-SSH <carlos.anezeh@me.com>
2021-04-26 09:50:35 -0500

    Create User model and validate with Minitest.

---
+++ test/models/user_test.rb
@@ -0,0 +1,19 @@
+require "test_helper"
+
+class UserTest < ActiveSupport::TestCase
+  test "User with a valid email should be valid" do
+    user = User.new(email: 'test@test.com', password_digest: 'test')
+    assert user.valid?
+  end
+
+  test 'User with invalid email should be invalid' do
+    user = User.new(email: 'test', password_digest: 'test')
+    assert_not user.valid?
+  end
+
+  test 'User with taken email should be valid' do
+    other_user = users(:one)
+    user = User.new(email: other_user.email, password_digest: 'test')
+    assert_not user.valid?
+  end
+end
